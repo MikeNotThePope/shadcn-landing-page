6a0ad1c3a3cef157dbaa3880f3b576e8dc8a7ef0
Sheikh Mubashir <smubashir655@gmail.com>
2025-11-06 00:26:58 +0500

    mobile responsev

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -8,7 +8,7 @@ export const Cta = () => {
      <h2 className="text-3xl md:text-4xl font-bold ">
       All Your
       <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-       {" "}
+       {" "} 
        Ideas & Concepts{" "}
       </span>
       In One Interface
